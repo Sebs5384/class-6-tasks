a8287d96bc4d6cfc0f27643d5c62f23f31b2df52
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-01-30 21:46:43 +0100

    creates js, html for task-2
    a small calculator which takes inputs from the family members and outputs their salaries

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# class-6-task
- class-6 for r/argentina-programa
+class-6-task-2 for r/argentina-programa
+
+In this task we develope a small web which captures the family members wage as inputs to calculate their minimum, maximum, average per month and average per year as ouputs.
---
+++ task-2/task-2.js
@@ -0,0 +1,103 @@
+document.querySelector("#button-add-member").onclick = function () {
+  addNewMember();
+  createCalculateButton();
+};
+
+document.querySelector("#button-remove-member").onclick = function () {
+  removeMember();
+};
+
+document.querySelector("#calculate-button").onclick = function () {
+  salaryResults();
+};
+
+function addNewMember() {
+  createMember();
+}
+
+function salaryResults() {
+  calculateResults();
+}
+
+function createMember() {
+  const $memberList = document.querySelectorAll(".input-list");
+  let number = $memberList.length + 1;
+  const $input = document.createElement("input");
+  $input.className = "input-list";
+  $input.type = "number";
+  $input.id = `member-${number}`;
+  $input.placeholder = `Entry member-${number} salary`;
+
+  const $br = document.createElement("br");
+  $br.id = `br-${number}`;
+
+  const $label = document.createElement("label");
+  $label.innerText = `Family member #${number}`;
+  $label.id = `label-${number}`;
+
+  const $member = document.querySelector("#members");
+  $member.appendChild($label);
+  $member.appendChild($input);
+  $member.appendChild($br);
+}
+
+function removeMember() {
+  const $memberList = document.querySelectorAll(".input-list");
+  let number = $memberList.length;
+  const $member = document.querySelector(`#member-${number}`);
+  const $br = document.querySelector(`#br-${number}`);
+  const $label = document.querySelector(`#label-${number}`);
+
+  $label.remove();
+  $member.remove();
+  $br.remove();
+
+  if (number === 1) {
+    removeCalculateButton();
+  }
+}
+
+function removeCalculateButton() {
+  const $calculateButton = document.querySelector("#calculate-salary-button");
+  $calculateButton.remove();
+}
+
+function createCalculateButton() {
+  if (!document.querySelector("#calculate-salary-button")) {
+    const $calculateButton = document.createElement("button");
+    $calculateButton.innerText = "Calculate";
+    $calculateButton.id = "calculate-salary-button";
+
+    const $button = document.querySelector("#calculate-button");
+    $button.appendChild($calculateButton);
+  }
+}
+
+function calculateResults() {
+  const $salaryList = document.querySelectorAll(".input-list");
+  const $results = document.querySelector("#salary-results");
+
+  let highestSalary = Number($salaryList[0].value);
+  let lowestSalary = Number($salaryList[0].value);
+  let salarySum = 0;
+  let existingMembers = 0;
+  for (let i = 0; i < $salaryList.length; i++) {
+    if ($salaryList[i].value !== "") {
+      let salary = Number($salaryList[i].value);
+
+      if (salary < lowestSalary) {
+        lowestSalary = salary;
+      } else if (salary > highestSalary) {
+        highestSalary = salary;
+      }
+      salarySum += salary;
+      existingMembers++;
+
+      const average = salarySum / existingMembers;
+      const monthlyAverage = average / 12;
+      $results.innerText = `The highest salary is ${highestSalary}, the lowest is ${lowestSalary} the anual average is ${average.toFixed(1)} and the monthly average is ${monthlyAverage.toFixed(2)}`;
+    } else if (existingMembers === 0) {
+      $results.innerText = `Introduce valid numbers`;
+    }
+  }
+}
