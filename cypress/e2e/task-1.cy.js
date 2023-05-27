66594e9cebd89706c466b366e44cee6244ba711a
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-05-27 03:07:52 -0600

    add test for the member inputs
    Testing out the errors that could have place in the member  inputs upon putting invalid values and then valid values.

--- cypress/e2e/task-1.cy.js
+++ cypress/e2e/task-1.cy.js
@@ -87,4 +87,52 @@ describe("Testing task-1-calculator", () => {
     cy.get(".error").should("not.exist");
     cy.get(".created-members").should("exist");
   });
+
+  it("Test errors in the member inputs", () => {
+    cy.visit(URL);
+
+    cy.get('input[name="members-quantity"]').type("4");
+    cy.get('button[name="quantity-submit"]').click();
+
+    cy.get(".created-members").should("exist");
+    cy.get("#member-2").type("3.4");
+    cy.get("#member-3").type("111111");
+    cy.get("#member-4").type("4");
+
+    cy.get('button[name="calculate-button"]').click();
+    cy.get(".error").should("be.visible");
+    cy.get("#error-1").should("have.text", "This field cannot be empty");
+    cy.get("#error-2").should("have.text", "This field doesn't accept decimals");
+    cy.get("#error-3").should("have.text", "This field should only contain a maximum of 3 characters");
+    cy.get("#error-4").should("have.text", "");
+
+    cy.get("#member-1").clear();
+    cy.get("#member-2").clear();
+    cy.get("#member-3").clear();
+    cy.get("#member-4").clear();
+
+    cy.get("#member-1").type("1");
+    cy.get("#member-2").type("2");
+    cy.get("#member-3").type("3");
+    cy.get("#member-4").type("4");
+
+    cy.get('button[name="calculate-button"]').click();
+    cy.get(".error").should("not.exist");
+    cy.get("#error-1").should("have.text", "");
+    cy.get("#error-2").should("have.text", "");
+    cy.get("#error-3").should("have.text", "");
+    cy.get("#error-4").should("have.text", "");
+
+    cy.get("#paragraph-1").should("have.text", "The youngest member on your family is 1 years old");
+    cy.get("#paragraph-2").should("have.text", "While the oldest is 4 years old");
+    cy.get("#paragraph-3").should("have.text", "And the average age in your family is 2.5.");
+
+    cy.get("button[name='restart-form-button']").click();
+    cy.get(".created-members").should("not.exist");
+    cy.get("#paragraph-1").should("have.text", "Welcome dear user");
+    cy.get("#paragraph-2").should("have.text", "This is a small program that calculates your family oldest, youngest and average age !");
+    cy.get("#paragraph-3").should("have.text", "Entry the number of members your family have to begin");
+
+    cy.get('input[name="members-quantity"]').clear();
+  });
 });
