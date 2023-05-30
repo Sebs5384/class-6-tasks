e9aff2cc75f76dc54dea22084ce3a6b337b363c7
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-05-30 02:48:33 -0600

    add second case of testing
    This will test the incorrect use of the task-2-calculator.

--- cypress/e2e/task-2.cy.js
+++ cypress/e2e/task-2.cy.js
@@ -32,7 +32,39 @@ describe("Testing task-2-calculator", () => {
     cy.get("#salary-results").should("not.be.visible");
   });
 
-  it("Test errors on the member inputs", () => {
+  it("Test the incorrect use of task-2-calculator", () => {
     cy.visit(URL);
+
+    for (let i = 0; i < 4; i++) {
+      cy.get('button[name="button-add-member"').click();
+      cy.get(`#member-${i + 1}`).should("exist");
+    }
+
+    cy.get('button[name="calculate-button"]').should("be.visible");
+
+    cy.get("#member-2").type("2.3");
+    cy.get("#member-3").type("hello-cypress");
+    cy.get("#member-4").type("12345678");
+
+    cy.get('button[name="calculate-button"]').click();
+    cy.get(".error").should("exist");
+
+    cy.get("#error-1").should("have.text", "This field cannot have a value of 0 or be empty");
+    cy.get("#error-2").should("have.text", "This field cannot have decimal values");
+    cy.get("#error-3").should("have.text", "This field cannot have a value of 0 or be empty");
+    cy.get("#error-4").should("have.text", "This field should only contain a maximum of 7 characters");
+
+    for (let i = 0; i < 4; i++) {
+      cy.get(`#member-${i + 1}`).clear();
+      cy.get(`#member-${i + 1}`).type(`${i + 1}`);
+    }
+
+    cy.get('button[name="calculate-button"]').click();
+    cy.get("#salary-results").should("be.visible");
+
+    cy.get("#highest-salary").should("have.text", "4");
+    cy.get("#lowest-salary").should("have.text", "1");
+    cy.get("#average-salary").should("have.text", "2.5");
+    cy.get("#monthly-salary").should("have.text", "0.2");
   });
 });
