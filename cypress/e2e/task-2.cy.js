8414a500b38aea735c775f43bd4e56773db831cd
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-05-30 02:00:44 -0600

    Updates first test
    this will make the first test more readable

--- cypress/e2e/task-2.cy.js
+++ cypress/e2e/task-2.cy.js
@@ -9,33 +9,30 @@ describe("Testing task-2-calculator", () => {
     }
 
     cy.get('button[name="calculate-button"]').should("be.visible");
-    cy.get("#member-1").should("exist");
-    cy.get("#member-2").should("exist");
-    cy.get("#member-3").should("exist");
-    cy.get("#member-4").should("exist");
 
-    cy.get("#member-1").type("1");
-    cy.get("#member-2").type("2");
-    cy.get("#member-3").type("3");
-    cy.get("#member-4").type("4");
+    for (let i = 0; i < 4; i++) {
+      cy.get(`#member-${i + 1}`).should("exist");
+      cy.get(`#member-${i + 1}`).type(`${i + 1}`);
+    }
 
     cy.get('button[name="calculate-button"]').click();
     cy.get("#salary-results").should("be.visible");
+
     cy.get("#highest-salary").should("have.text", "4");
     cy.get("#lowest-salary").should("have.text", "1");
     cy.get("#average-salary").should("have.text", "2.5");
     cy.get("#monthly-salary").should("have.text", "0.2");
 
-    cy.get('button[name="button-remove-member"]').click();
-    cy.get("member-4").should("not.exist");
-    cy.get('button[name="button-remove-member"]').click();
-    cy.get("member-3").should("not.exist");
-    cy.get('button[name="button-remove-member"]').click();
-    cy.get("member-2").should("not.exist");
-    cy.get('button[name="button-remove-member"]').click();
-    cy.get("member-1").should("not.exist");
+    for (let i = 3; i >= 0; i--) {
+      cy.get('button[name="button-remove-member"]').click();
+      cy.get(`#member-${i + 1}`).should("not.exist");
+    }
 
     cy.get("#button-calculate").should("not.be.visible");
     cy.get("#salary-results").should("not.be.visible");
+  });
+
+  it("Test errors on the member inputs", () => {
+    cy.visit(URL);
   });
 });
